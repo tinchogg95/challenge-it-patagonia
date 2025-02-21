d05b41bd39dc0c4633e0856bda6bb1280dc74d0d
tinchogg95 <tinchogimenez95@gmail.com>
2025-02-21 17:41:21 -0300

    final commit

--- src/main/java/com/itPatagonia/challenge/application/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/itPatagonia/challenge/application/exceptions/GlobalExceptionHandler.java
@@ -3,10 +3,10 @@ package com.itPatagonia.challenge.application.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
--- src/main/java/com/itPatagonia/challenge/infraestructure/dto/EmpresaDTO.java
+++ src/main/java/com/itPatagonia/challenge/infraestructure/dto/EmpresaDTO.java
@@ -1,31 +1,35 @@
 package com.itPatagonia.challenge.infraestructure.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
+@Schema(description = "DTO para la creación o actualización de una empresa")
 public class EmpresaDTO {
-    private final static String CUIT_ERROR = "El CUIT es obligatorio";
-    private final static String RAZON_SOCIAL_ERROR = "La razón social es obligatoria";
-    private final static String FECHA_ADHESION_ERROR = "La fecha de adhesión es obligatoria";
+    private static final String CUIT_ERROR = "El CUIT es obligatorio";
+    private static final String RAZON_SOCIAL_ERROR = "La razón social es obligatoria";
+    private static final String FECHA_ADHESION_ERROR = "La fecha de adhesión es obligatoria";
     public EmpresaDTO(String cuit, String razonSocial, LocalDate fechaAdhesion) {
         this.cuit = cuit;
         this.razonSocial = razonSocial;
         this.fechaAdhesion = fechaAdhesion;
     }
-
     public EmpresaDTO() {
         super();
     }
 
     @NotBlank(message = CUIT_ERROR)
+    @Schema(description = "CUIT de la empresa", example = "30-12345678-9", required = true)
     private String cuit;
 
     @NotBlank(message = RAZON_SOCIAL_ERROR)
+    @Schema(description = "Razón social de la empresa", example = "Empresa de Prueba", required = true)
     private String razonSocial;
 
     @NotNull(message = FECHA_ADHESION_ERROR)
+    @Schema(description = "Fecha de adhesión de la empresa", example = "2023-10-01", required = true)
     private LocalDate fechaAdhesion;
 }
